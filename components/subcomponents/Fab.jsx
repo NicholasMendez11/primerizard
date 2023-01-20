0cb95aab1f65167f7f972fd4e5577cfd2410c5cc
Nicholas Mendez <nicholasmeeendez@gmail.com>
2023-01-20 13:19:33 -0400

    Update Fab.jsx

--- components/subcomponents/Fab.jsx
+++ components/subcomponents/Fab.jsx
@@ -4,6 +4,7 @@ import whatsapp from "../../public/whatsapp.json";
 function Fab() {
   return (
     <a
+    rel="noreferrer"
       target="_blank"
       href="https://chat.whatsapp.com/GXKpnNfotZzD4AOxlQD83W"
       className="fixed z-90 bottom-10 right-8 bg-transparent w-30 h-30 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-[#b5919f25] hover:drop-shadow-2xl hover:animate-pulse duration-300"
