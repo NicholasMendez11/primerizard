77d7b6797ca5d7e3b5a48bc869e23ce602fd7728
Nicholas Mendez <nicholasmeeendez@gmail.com>
2023-06-07 19:27:53 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,8 @@ import "../styles/globals.css";
 import { Layout } from "../components";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
-import { useEffect, useState } from "react";
-import LoadingPage from "../components/LoadingPage";
 
 function MyApp({ Component, pageProps }) {
-  const [isLoading, setIsLoading] = useState(true);
-
   return (
     <StateContext>
       <Layout>
